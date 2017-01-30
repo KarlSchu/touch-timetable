de3b00f6e4dea6db97199eb147c990fdcd94e54e
ksc <ksc@pegasusschule.ch>
2017-01-30 06:15:04 +0100

    Dynamic HTML creation basics

--- assets/timetable.js
+++ assets/timetable.js
@@ -1,3 +1,29 @@
+var lessonDef = ['L1', 'L2', 'L3', 'L4', 'L5', 'L6', 'L7', 'L8'];
+var lessonTimeDef = ['8:00-9:50', '10:10-12:00'];
+var dayDef = ['mo', 'di', 'mi', 'do', 'fr'];
+
+// var groupDef = ['A', 'B', 'C', 'D', 'E'];
+// var groupNameDef = ['GrA1', 'GrA2', 'GrA3', 'GrB', 'GrC', 'GrD'];
+var groupDef = { 'A': 'GrA1', 'B': 'GrA', 'C': 'GrA3', 'D': 'GrB', 'E': 'GrC' };
+
+// var teacherDef = ['T0', 'T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8', 'T9', 'T10', 'T11'];
+// var teacherNameDef = ['KsC', 'KSC', 'UWO', 'SBO', 'CES', 'XXX', 'KRU', 'HME', 'SKU', 'AST', 'ISC', 'YYY'];
+var teacherDef = {
+    'T0': 'KsC', 'T1': 'KSC', 'T2': 'UWO', 'T3': 'SBO', 'T4': 'CES', 'T5': 'XXX'
+    , 'T6': 'KRU', 'T7': 'HME', 'T8': 'SKU', 'T9': 'AST', 'T10': 'ISC', 'T11': 'YYY'
+}
+
+function initTimeTableGrid() {
+    var nodeTr = document.createElement("tr");
+     $.each(groupDef, function (key, value) {
+        var nodeTd = document.createElement("td");
+        nodeTd.appendChild(document.createTextNode(value));
+        // node.setAttribute('id', key);
+        nodeTd.setAttribute('class', 'headerGroup');
+        $("#headerGroup"+ky).append(nodeTd);
+    });
+}
+
 function clickOnLesson(event) {
     var target = event.currentTarget;
     var lessonId = target.parentNode.id;
@@ -14,24 +40,16 @@ function handleOk(event) {
     var field = $("#" + fieldId)[0];
     field.children[0].children[1].innerHTML = $("#subject").val();
     field.children[0].children[2].innerHTML = $("#room").val();
-    saveData();
+    saveData(); idxDropLesson
 }
-
-// function extendContent(origContent) {
-//     var newContent = prompt("Add content", origContent);
-//     if (newContent != null) {
-//         return newContent;
-//     }
-//     return origContent;
-// }
 
 function initTimeTable() {
     var timeTable = new Object();
-    ['mo', 'di', 'mi', 'do', 'fr'].forEach(function (weekDay) {
+    dayDef.forEach(function (weekDay) {
         timeTable[weekDay] = new Object();
-        ['A', 'B', 'C', 'D'].forEach(function (groupNr) {
+        groupDef.forEach(function (groupNr) {
             timeTable[weekDay][groupNr] = new Object();
-            ['L1', 'L2', 'L3', 'L4', 'L5', 'L6', 'L7', 'L8'].forEach(function (lessonNr) {
+            lessonDef.forEach(function (lessonNr) {
                 timeTable[weekDay][groupNr][lessonNr] = '';
             });
         });
@@ -41,19 +59,19 @@ function initTimeTable() {
 
 function fetchDataFromDOM() {
     var timeTable = initTimeTable();
-    ['mo', 'di', 'mi', 'do', 'fr'].forEach(function (weekDay) {
-        ['A', 'B', 'C', 'D'].forEach(function (groupNr) {
-            ['L1', 'L2', 'L3', 'L4', 'L5', 'L6', 'L7', 'L8'].forEach(function (lessonNr) {
+    dayDef.forEach(function (weekDay) {
+        groupDef.forEach(function (groupNr) {
+            lessonDef.forEach(function (lessonNr) {
                 timeTable[weekDay][groupNr][lessonNr] = '';
-                var e1List = $("#" + weekDay + groupNr + lessonNr)
-                if (e1List.length > 0) {
-                    var e2List = e1List[0].childNodes;
-                    if (e2List.length > 0) {
+                var level1List = $("#" + weekDay + groupNr + lessonNr)
+                if (level1List.length > 0) {
+                    var level2List = level1List[0].childNodes;
+                    if (level2List.length > 0) {
                         var content = new Object();
-                        content.id = e2List[0].id
-                        content.teacher = e2List[0].children[0].innerHTML
-                        content.subject = e2List[0].children[1].innerHTML
-                        content.room = e2List[0].children[2].innerHTML
+                        content.id = level2List[0].id
+                        content.teacher = level2List[0].children[0].innerHTML
+                        content.subject = level2List[0].children[1].innerHTML
+                        content.room = level2List[0].children[2].innerHTML
                         timeTable[weekDay][groupNr][lessonNr] = content;
                         //timeTable[weekDay][groupNr][lessonNr] = e2List[0].id;
                     }
@@ -65,9 +83,9 @@ function fetchDataFromDOM() {
 }
 
 function reloadDataIntoDOM(timeTable) {
-    ['mo', 'di', 'mi', 'do', 'fr'].forEach(function (weekDay) {
-        ['A', 'B', 'C', 'D'].forEach(function (groupNr) {
-            ['L1', 'L2', 'L3', 'L4', 'L5', 'L6', 'L7', 'L8'].forEach(function (lessonNr) {
+    dayDef.forEach(function (weekDay) {
+        groupDef.forEach(function (groupNr) {
+            lessonDef.forEach(function (lessonNr) {
                 var target = $("#" + weekDay + groupNr + lessonNr)[0];
                 if (target == undefined) {
                     alert("Error: Expected timetable element '" + weekDay + groupNr + lessonNr + "' not found!");
@@ -126,28 +144,27 @@ function recalcAll() {
     var teachers = $(".spring");
     var elements = $(".dropLesson");
     // init output fields
-    ['A', 'B', 'C', 'D'].forEach(function (groupNr) {
-        $("#Gr" + groupNr).text('_');
-        ['T0', 'T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8', 'T9', 'T10', 'T11'].forEach(function (taecherNr) {
-            $("#" + taecherNr + "G" + groupNr).text('-');
-            $("#" + taecherNr + "G" + groupNr).text('-');
-            $("#" + taecherNr + "G" + groupNr).text('-');
+    groupDef.forEach(function (groupNr) {
+        $("#Gr" + groupNr).text('-');
+        teacherDef.forEach(function (taecherNr) {
             $("#" + taecherNr + "G" + groupNr).text('-');
         });
     });
+    // claculate output fields
     var groupCounter = new Object();
     groupCounter.A = 0;
     groupCounter.B = 0;
     groupCounter.C = 0;
     groupCounter.D = 0;
+    groupCounter.E = 0;
     // calc lessons per teacher
     elements.each(function (idxDropLesson, dropLesson) {
         if (dropLesson.childNodes.length > 0) {
-            var regExGroups = /..([ABCD])L[0-9]/.exec(dropLesson.id);
+            var regExGroups = /..([ABCDE])L[0-9]/.exec(dropLesson.id);
             if (regExGroups.length > 0) {
                 var regExGroup = regExGroups[1];
                 dropLesson.childNodes.forEach(function (child) {
-                    ['T0', 'T1', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7', 'T8', 'T9', 'T10', 'T11'].forEach(function (taecherNr) {
+                    teacherDef.forEach(function (taecherNr) {
                         if (child.id == taecherNr) {
                             var lessons;
                             if (isNaN($("#" + taecherNr + "G" + regExGroup).text())) {
@@ -170,6 +187,7 @@ function recalcAll() {
     $("#GrB").text(groupCounter.B);
     $("#GrC").text(groupCounter.C);
     $("#GrD").text(groupCounter.D);
+    $("#GrE").text(groupCounter.E);
 }
 
 function loadDataFromStore() {
